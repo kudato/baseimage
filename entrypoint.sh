5bd20609328585a411231fec770c97a82887c9c5
Alexander Shevchenko <kudato@me.com>
2019-04-30 00:56:44 +0300

    Add entrypoint.sh wrapper.

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# checking if vault is enabled
+if [ "${VAULT_ENABLED}" == "True" ]; then
+    source /usr/bin/vault.sh
+fi
+
+# checking the needexec to run the init script
+# be careful, the script run as root,
+# but the app will be run as APP_USER
+if [ -n "${INIT_SH}" ]; then
+	source /usr/bin/init.sh
+fi
+
+
+# timezone setup
+if [ -f "/etc/localtime" ]; then rm /etc/localtime; fi
+cp /usr/share/zoneinfo/${TZ} /etc/localtime
+echo "${TZ}" > /etc/timezone
+
+# run using su-exec
+# more details - https://github.com/ncopa/su-exec
+exec su-exec ${APP_USER} $@
