a221c18ab9ce019641062b33ceb69db00bd7f00b
Alexander Shevchenko <kudato@me.com>
2019-05-15 05:19:23 +0300

    Refactoring the init script.

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-# checking if exist script
-if [ ! -f "${INIT}" ]; then
-    echo "ERROR! You have defined init script, but it is not found."
-    exit 2
-fi
+init_scripts() {
+    local result="${INIT}"
+    for script in $(env | grep INIT | grep SH | sort)
+    do
+      local path=$(echo "$script" | sed -r 's/[^=]+//' | sed 's|=||g')
+      local result="${path} ${result}"
+    done
+    echo $result
+}
 
-# run init script
-chmod +x "${INIT}"
-${INIT_SH}
+check_file_exist() {
+    if [ ! -f "${1}" ]; then
+        echo "ERROR! ${1} is defined, but it is not found."
+        exit 2
+    fi
+}
 
-# checking the exit code of the init script
-if [ "$?" -ne "0" ]; then
-  echo "${INIT} failed"
-  exit 1
-fi
+init_exec() {
+    check_file_exist ${1}
+    chmod +x ${1}
+    ${1}
+    if [ "$?" -ne "0" ]; then
+        echo "${1} exitcode is $?, executed failed"
+        exit 1
+    fi
+}
+
+for i in $(init_scripts)
+do
+    init_exec $i
+done
