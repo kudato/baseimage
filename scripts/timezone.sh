7ea0c4ec3baa960539f954e6acb7219b3b2fba5d
Alexander Shevchenko <kudato@me.com>
2019-05-15 05:23:19 +0300

    Fix format.

--- scripts/timezone.sh
+++ scripts/timezone.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set_tz() {
-	if [ -f "/etc/localtime" ]; then rm /etc/localtime; fi
-	cp /usr/share/zoneinfo/${1} /etc/localtime
-	echo "${1}" > /etc/timezone
+    if [ -f "/etc/localtime" ]; then rm /etc/localtime; fi
+    cp /usr/share/zoneinfo/${1} /etc/localtime
+    echo "${1}" > /etc/timezone
 }
 
 set_tz ${TZ}
