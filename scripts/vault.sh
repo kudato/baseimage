25d33af2d63d340d695ae75c1fa85cd1585e1b03
Alexander Shevchenko <kudato@me.com>
2019-07-15 02:59:37 +0300

    Request and parsing response merge to a single function.

--- scripts/vault.sh
+++ scripts/vault.sh
@@ -1,71 +1,58 @@
 #!/bin/bash
 
-# checks is set required vars
-if [ -z "${VAULT_TOKEN}" ] \
- | [ -z "${VAULT_ADDR}" ] \
- | [ -z "${VAULT_PATH}" ]
-then
-    echo "ERROR! Vault is enabled, but required variables are not set."
-    exit 2
-fi
+requestEnv.Vault() {
+    # ------------------------------------------
+    # Get data from Vault KV storage
+    # ------------------------------------------
+    # $* - None
+    # ------------------------------------------
+    # return - NAME___VALUE like array
+    # ------------------------------------------
+    # required env vars:
+    #   - VAULT_KV_VERSION
+    #   - VAULT_TOKEN
+    #   - VAULT_ADDR
+    #   - VAULT_PATH
+    # ------------------------------------------
 
-# default verison
-if [ -z "${VAULT_KV_VERSION}" ]
-then
-    export VAULT_KV_VERSION=1
-fi
+    Vault.request() {
+        curl -s \
+             -H "Accept: application/json" \
+             -H "X-Vault-Token: ${VAULT_TOKEN}" \
+             -X GET ${VAULT_ADDR}/"${1}"
+    }
 
-vault_url() {
-    local base
-
-    if [ "${VAULT_KV_VERSION}" == "1" ]
+    local uri=v1/secret
+    if [[ "${VAULT_KV_VERSION}" == "1" ]]
     then
-        base=${VAULT_ADDR}/v1/secret/${VAULT_PATH}
-
-    elif [ "${VAULT_KV_VERSION}" == "2" ]
+        Vault.request "${uri}/${VAULT_PATH}/${ENVIRONMENT}" \
+        | jq '.data | to_entries | map([.key, .value]|join("___"))|join(" ")'
+    elif [[ "${VAULT_KV_VERSION}" == "2" ]]
     then
-        base=${VAULT_ADDR}/v1/secret/data/${VAULT_PATH}
+        Vault.request "${uri}/data/${VAULT_PATH}/${ENVIRONMENT}" \
+        | jq '.data | .data | to_entries | map([.key, .value]|join("___"))|join(" ")'
     fi
-
-    if [ -n "${ENVIRONMENT}" ]
-    then
-        base=$base/${ENVIRONMENT}
-    fi
-
-    echo "$base"
 }
 
-vault_request() {
-    local response
-    response=$(curl -s \
-        -H "Accept: application/json" \
-        -H "X-Vault-Token: ${VAULT_TOKEN}" \
-        -X GET "$(vault_url)")
-
-    echo "$response"
-}
-
-if [ "${VAULT_KV_VERSION}" == "1" ]
+if [[ -z "${VAULT_ADDR}" ]] \
+ | [[ -z "${VAULT_PATH}" ]]
 then
-    secret_data=$(vault_request \
-                  | jq '.data | to_entries | map([.key, .value]|join("___"))|join(" ")')
-
-elif [ "${VAULT_KV_VERSION}" == "2" ]
-then
-    secret_data=$(vault_request \
-                  | jq '.data | .data | to_entries | map([.key, .value]|join("___"))|join(" ")')
-
-else
-    echo "ERROR! Wrong version of the secrets engine."
-    echo "Please set version 1 or 2 to VAULT_KV_VERSION variable"
-    exit 2
-fi
-
-if [ "${secret_data}" == "" ]; then
-    echo "ERROR! Received data has failed or either Vault is empty"
+    echo "ERROR! Vault is enabled but not configured."
     exit 1
-fi
+else
+	if [[ -z "${VAULT_KV_VERSION}" ]]
+    then
+        export VAULT_KV_VERSION=1
+    fi
 
-for var in ${secret_data:1:${#secret_data}-2}; do
-    export ${var%___*}=${var#*___}
-done
+	VAULT_RESPONSE=$(requestEnv.Vault)
+    if [[ -z "${VAULT_RESPONSE}" ]]
+    then
+        echo "ERROR! Failed retrieving data from Vault."
+        exit 1
+    fi
+	for var in ${VAULT_RESPONSE:1:${#VAULT_RESPONSE}-2}; do
+	    export "${var%___*}"="${var#*___}"
+	done
+    unset VAULT_RESPONSE
+fi
