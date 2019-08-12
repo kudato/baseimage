e16ae1778a556cb613c9891bccad30ba314deeac
Alexander Shevchenko <kudato@me.com>
2019-08-13 02:44:29 +0300

    Add run tests script.

---
+++ run_tests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Install this
+for i in \
+    lib.sh \
+    vault.sh \
+    entrypoint.sh \
+    healthcheck.sh
+do
+    if [[ -f "/usr/bin/${i}" ]]
+    then sudo rm "/usr/bin/${i}"; fi
+    sudo cp "scripts/${i}" "/usr/bin/${i}"
+    sudo chmod +x "/usr/bin/${i}"
+done
+
+# Test
+cd tests || exit
+for testf in $(echo ./test_*)
+do
+    chmod +x "${testf}"
+    ${testf}
+done
