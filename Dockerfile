31951d4054fe44b179adc69172133dde5d6a2532
Alexander Shevchenko <kudato@me.com>
2019-08-31 16:51:11 +0300

    Refactoring.

--- Dockerfile
+++ Dockerfile
@@ -3,21 +3,21 @@ FROM ${image}
 
 ARG cmd
 ARG init=entrypoint.sh
+
 ENV IMAGE_CMD=${cmd} \
     IMAGE_INIT=${init} \
     LANG=en_US.UTF-8 \
     TZ=UTC
 
-
 COPY scripts/entrypoint.sh \
      scripts/environment.sh \
      scripts/healthcheck.sh \
-     scripts/${init} \
      scripts/lib.sh \
+     scripts/${init} \
      /usr/bin/
 
-RUN \
-    delgroup ping \
+RUN chmod +x /usr/bin/*.sh \
+    && delgroup ping \
     && addgroup -g 998 ping \
     && apk add --no-cache \
         bash \
@@ -26,8 +26,7 @@ RUN \
         curl \
         ca-certificates \
         su-exec \
-        tini \
-    && chmod +x /usr/bin/*.sh
+        tini
 
 HEALTHCHECK \
     --start-period=30s \
@@ -37,4 +36,3 @@ HEALTHCHECK \
     CMD /usr/bin/healthcheck.sh
 
 ENTRYPOINT [ "tini", "--", "/usr/bin/entrypoint.sh" ]
-
--- build.sh
+++ build.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
-# Required vars:
-#   Name=Default
-#   DOCKER_HUB_IMAGE=
-#   TAGS
-#   SOURCE
-#   IMAGE
-#   COMMIT_SHA
-#
+# shellcheck disable=SC1091,SC2181
+
+# Build docker image from ./Dockerfile
+# set DOCKER_HUB_IMAGE for push image to docker hub
+
+# Variables:
+#   required:
+#      SOURCE
+#      TAGS
+#   optional:
+#      IMAGE_INIT
+#      IMAGE_CMD
 
 source scripts/lib.sh
 
+#   DOCKER_HUB_IMAGE
+#   TRAVIS_COMMIT
+#   TAGS
 set_tags() {
     declare -a tags
     OLDIFS=${IFS}; IFS=","
@@ -24,40 +31,32 @@ set_tags() {
     unset tags
 }
 
-build_and_push() {
-    local name
-    defaultEnv TRAVIS_COMMIT=dev
-    defaultEnv IMAGE_INIT=entrypoint.sh
-    name=$(replace "${SOURCE}" ":" "-")
-    if [[ -n "${IMAGE_CMD}" ]] \
-    && [[ -n "${IMAGE_INIT}" ]]
-    then
-        docker build -t "${name}" \
-            --build-arg image="${SOURCE}" \
-            --build-arg cmd="${IMAGE_CMD}" \
-            --build-arg init="${IMAGE_INIT}" .
-
-    else
-        docker build -t "${name}" \
-            --build-arg image="${SOURCE}" .
-    fi
-
-    if [[ "${?}" != "0" ]]
-    then
-        echo "Build failed"
-        exit 1
-    fi
-    if [[ -n "${DOCKER_HUB_IMAGE}" ]]
-    then
-        for i in $(set_tags "${name}")
-        do
-            docker push "${DOCKER_HUB_IMAGE}:${i}"
-            docker push "${DOCKER_HUB_IMAGE}:${i}-${TRAVIS_COMMIT:0:7}"
-        done
-    fi
-}
+defaultEnv TRAVIS_COMMIT=dev
+defaultEnv IMAGE_INIT=entrypoint.sh
+defaultEnv IMAGE_CMD="/bin/bash"
+name=$(replace "${SOURCE}" ":" "-")
 
 echo "Building.."
-build_and_push
+docker build -t "${name}" \
+    --build-arg image="${SOURCE}" \
+    --build-arg cmd="${IMAGE_CMD}" \
+    --build-arg init="${IMAGE_INIT}" .
+
+if [[ "${?}" != "0" ]]
+then
+    echo "Build failed"
+    exit 1
+fi
 echo "Build complete"
+
+if [[ -n "${DOCKER_HUB_IMAGE}" ]]
+then
+    for i in $(set_tags "${name}")
+    do
+        docker push "${DOCKER_HUB_IMAGE}:${i}"
+        docker push "${DOCKER_HUB_IMAGE}:${TRAVIS_COMMIT:0:7}-${i}"
+    done
+fi
+
 exit 0
+
